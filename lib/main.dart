2945257404753b47451dea541e58fc55d64668e6
benoît <bra@synapsia.fr>
2025-10-07 20:00:34 +0200

    remove tests print

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:advent_of_code/data/interceptor/interceptor_provider.dart';
 import 'package:advent_of_code/dotenv.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +7,4 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Environment.load();
   runApp(const App());
-  print(InterceptorProvider.authentication.hashCode);
-  print(InterceptorProvider.authentication.hashCode);
 }
