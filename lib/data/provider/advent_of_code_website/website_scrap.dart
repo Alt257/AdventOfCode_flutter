1c40b228520ae961bbfccaa6141c25f21254d7bc
benoît <bra@synapsia.fr>
2025-10-08 00:38:09 +0200

    #REFAC abstract HttpDataProvider get/post

--- lib/data/provider/advent_of_code_website/website_scrap.dart
+++ lib/data/provider/advent_of_code_website/website_scrap.dart
@@ -1,35 +1,38 @@
+import 'package:advent_of_code/CORE/data/http_data_provider.dart';
 import 'package:advent_of_code/data/interceptor/interceptor_provider.dart';
-import 'package:dio/dio.dart';
 import 'package:html/parser.dart';
 
 part 'dto.dart';
 
-final class AdventOfCodeWebsiteDataProvider {
-  final String url;
-  final http = Dio();
+final class AdventOfCodeWebsiteDataProvider extends HttpDataProvider {
 
-  AdventOfCodeWebsiteDataProvider(): url = 'https://adventofcode.com' {
-    http.interceptors..add(InterceptorProvider.authentication)
-                     // ..add(LogInterceptor())
-    ;
-  }
+  AdventOfCodeWebsiteDataProvider(): super(
+      url: 'https://adventofcode.com',
+      globalInterceptors: [
+        // LogInterceptor(),
+      ],
+  );
 
   Future<AdventOfCodeWebsite_getDescription_response> getDescription({ required int year, required int day }) =>
-      http.get('$url/$year/day/$day')
-          .then((response) {
-            final document = parse(response.data);
-            return AdventOfCodeWebsite_getDescription_response(
-                description: document.getElementsByClassName('day-desc').first.text,
-            );
-          });
+      get('/$year/day/$day',
+        interceptors: []
+      )
+      .then((response) {
+        final document = parse(response.data);
+        return AdventOfCodeWebsite_getDescription_response(
+            description: document.getElementsByClassName('day-desc').first.text,
+        );
+      });
 
   Future<AdventOfCodeWebsite_getInput_response> getInput({ required int year, required int day }) =>
-      http.get('$url/$year/day/$day/input')
-          .then((response) {
-            final document = parse(response.data);
-            return AdventOfCodeWebsite_getInput_response(
-              input: document.body!.text,
-            );
-          });
+      get('/$year/day/$day/input',
+        interceptors: [InterceptorProvider.authentication]
+      )
+      .then((response) {
+        final document = parse(response.data);
+        return AdventOfCodeWebsite_getInput_response(
+          input: document.body!.text,
+        );
+      });
 }
 
