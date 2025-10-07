983eafb45cc20f8658a7608eb80d96833ba3aa5c
benoît <bra@synapsia.fr>
2025-10-07 18:24:33 +0200

    #REFAC InterceptorProvider ensure single instance of AuthInterceptor

--- lib/data/interceptor/interceptor_provider.dart
+++ lib/data/interceptor/interceptor_provider.dart
@@ -4,6 +4,14 @@ import 'package:dio/dio.dart';
 import 'authentication.dart';
 
 class InterceptorProvider {
+  static final _interceptors = <String, Interceptor>{};
 
-  static Interceptor get authentication => AuthInterceptor(Environment.read('COOCKIE_SESSION'));
+  static Interceptor get authentication {
+    final instance = AuthInterceptor(Environment.read('COOCKIE_SESSION'));
+    final key = instance.runtimeType.toString();
+    if(!_interceptors.containsKey(key)) {
+      _interceptors.addAll({key: instance});
+    }
+    return _interceptors[key]!;
+  }
 }
