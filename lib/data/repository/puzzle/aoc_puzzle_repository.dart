6f2f9a64bb9a3358e52cb9659e046a00234a60f4
benoît <bra@synapsia.fr>
2025-10-08 03:46:35 +0200

    #REFAC data/provider-->data/data_provider

--- lib/data/repository/puzzle/aoc_puzzle_repository.dart
+++ lib/data/repository/puzzle/aoc_puzzle_repository.dart
@@ -1,4 +1,4 @@
-import 'package:advent_of_code/data/provider/advent_of_code_website/website_scrap.dart';
+import 'package:advent_of_code/data/data_provider/advent_of_code_website/website_scrap.dart';
 import 'package:advent_of_code/data/repository/puzzle/i_puzzle_repository.dart';
 import 'package:advent_of_code/entity/puzzle/puzzle.dart';
 
