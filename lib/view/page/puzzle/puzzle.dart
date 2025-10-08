4b217e0a66112fcc66c9f719845e737ddf6cee88
benoît <bra@synapsia.fr>
2025-10-08 03:33:38 +0200

    replace return to home button

--- lib/view/page/puzzle/puzzle.dart
+++ lib/view/page/puzzle/puzzle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 final class PuzzlePage extends StatelessWidget {
   const PuzzlePage({super.key});
@@ -9,6 +10,7 @@ final class PuzzlePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('year/day'),
         centerTitle: true,
+        leading: IconButton(onPressed: () => context.go('/home'), icon: Icon(Icons.arrow_back)),
       ),
 
       body: Center(
